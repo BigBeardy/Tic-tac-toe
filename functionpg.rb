8826e0c77200d569a23f307564c831571f8a06ec
BigBeardy <westderick70@gmail.com>
2017-09-27 10:57:39 -0400

    added more tests

--- functionpg.rb
+++ functionpg.rb
@@ -6,6 +6,23 @@ def update_board(board,player,choice)
 	board
 end
 
+def movepick(player,choice)
+    board = ["1","x","3","4","5","6","7","8","9"]
+end
+
+def move(player,choice)
+    board = ["1","2","3","4","5","6","7","8","9"]  
+    board[choice - 1] = player
+
+    board
+end  
+
+def makemove(board,player,choice)  
+    board[choice - 1] = player
+
+    board
+end
+
 def valid_space(board,choice)
 	choice = choice-1
 	if choice > 9
@@ -19,11 +36,41 @@ def valid_space(board,choice)
 	end
 end
 
-def changing_players
+def change_players(player)
 	if player == "x"
 		player = "o"
 	else player = "x"
 	end
+end
+
+def winningcombos(board)
+
+    [[board[0], board[1], board[2]],
+     [board[0], board[4], board[8]],
+     [board[0],board[3], board[6]],
+     [board[3], board[4], board[5]],
+     [board[6], board[7], board[8]],
+     [board[6], board[4], board[2]],
+     [board[1], board[4], board[7]],
+     [board[2], board[5], board[8]]]
+	# 
+end
+
+def winning(board,player)
+	emp_array =[]
+	winningcombos(board).each do |combos|
+		if combos[0] == player && combos[1] == player && combos[2] == player
+			emp_array << true
+		else
+			emp_array << false
+		end
+	end
+	emp_array.include?(true)
+end
+
+# def samesign(winning)
+# 	if combos[0] == combos[1]
+
 
 
 
--- test.rb
+++ test.rb
@@ -8,35 +8,42 @@ class TestArr < Minitest::Test
 	  choice = 2 
 	  assert_equal(["1","x","3","4","5","6","7","8","9"],update_board(board,player,choice))
 	end
+	
 	def test_update_board5
 	  board = ["1","x","3","4","5","6","7","8","9"]
 	  player = "x"
 	  choice = 5
 	  assert_equal(["1","x","3","4","x","6","7","8","9"],update_board(board,player,choice))
 	end
+	
 	def test_update_board7
 	  board = ["1","x","3","4","5","6","7","8","9"]
 	  player = "x"
 	  choice = 7
 	  assert_equal(["1","x","3","4","5","6","x","8","9"],update_board(board,player,choice))
 	end
+	
 	def test_valid_space2
 		board = ["1","x","3","4","x","6","7","8","9"]
 		choice = 2
 		assert_equal(false,valid_space(board,choice))
 	end
+	
 	def test_falsetest
 		board = ["1","x","3","4","x","6","7","8","9"]
 		choice = 6
 		assert_equal(true,valid_space(board,choice))
-	end
-	def test_changeplayers
+	end 
+
+	def test_change_players
 		player = "x"
-		assert_equal("o"test_changeplayers(player))
+		assert_equal("o",change_players(player))
 		true
-	# def test_winning
-	# 	board = ["1","x","3","4","x","6","7","8","9"]
-	# 		true
-	# def choice_taken
-	# 	if choice
+	end 
+	def test_winning
+		board = ["x","x","x","4","x","6","7","8","9"]
+		player ="x"
+		assert_equal(true, winning(board,player))
+	end
+	
  end 
